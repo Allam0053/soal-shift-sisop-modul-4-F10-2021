91d677bda280aaf0376bec0bb7815006f2ad18db
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2021-06-04 08:48:34 +0700

    fungsi rot13 (Tinggal geser ke kanan 13 huruf)

--- SinSeiFS_F10.c
+++ SinSeiFS_F10.c
@@ -10,12 +10,33 @@ void atbash(char kode[])
 		if(isalpha(kode[i]))
 		{
 			if(islower(kode[i]))
+			kode[i] += (27 - ((kode[i] - 96) * 2));
+			else if(isupper(kode[i]))
+			kode[i] += (27 - ((kode[i] - 64) * 2));
+		}
+	}
+}
+
+void kodes(char kode[])
+{
+	int i;
+	for(i = 0; i < strlen(kode); i++)
+	{
+		if(isalpha(kode[i]))
+		{
+			if(islower(kode[i]))
 			{
-				kode[i] += (27 - ((kode[i] - 96) * 2));
+				if(kode[i] <= 109)
+				kode[i] += 13;
+				else
+				kode[i] -= 13;
 			}
 			else if(isupper(kode[i]))
 			{
-				kode[i] += (27 - ((kode[i] - 64) * 2));
+				if(kode[i] <= 77)
+				kode[i] += 13;
+				else
+				kode[i] -= 13;
 			}
 		}
 	}
