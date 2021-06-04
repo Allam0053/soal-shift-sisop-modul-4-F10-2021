b8ce13c8f23b8fbd0073585de896c63aaf7a8ae6
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2021-06-04 08:42:56 +0700

    fungsi atbash

--- SinSeiFS_F10.c
+++ SinSeiFS_F10.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+void atbash(char kode[])
+{
+	int i;
+	for(i = 0; i < strlen(kode); i++)
+	{
+		if(isalpha(kode[i]))
+		{
+			if(islower(kode[i]))
+			{
+				kode[i] += (27 - ((kode[i] - 96) * 2));
+			}
+			else if(isupper(kode[i]))
+			{
+				kode[i] += (27 - ((kode[i] - 64) * 2));
+			}
+		}
+	}
+}
+
 int main(int argc, char const *argv[]) {
   
   return 0;
